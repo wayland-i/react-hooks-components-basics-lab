455ab5279db467c58095bb8820c978321fcb139e
Ian Iversen <ianiversen1@gmail.com>
2022-07-18 10:54:23 -0400

    commit tree d1e601c93e30816e6b58342d17968f5801a68e84

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -3,6 +3,7 @@ import { render, screen } from "@testing-library/react";
 
 import App from "../components/App";
 
+
 test("renders without errors", () => {
   expect(() => render(<App />)).not.toThrow();
 });
@@ -14,7 +15,6 @@ test("renders the correct child components", () => {
     Uncomment the line below to see the DOM elements being returned
     by the App component in your terminal when you run the tests
   */
-  // screen.debug();
 
   expect(container.querySelector("nav")).toBeInTheDocument();
   expect(container.querySelector("#home")).toBeInTheDocument();
